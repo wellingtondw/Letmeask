6e2ce054ae1d525e39c5914854e6dd6c1b773432
Wellington de Lima <wellington.map@hotmail.com>
2021-06-22 08:42:56 -0300

    create new room screen base

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,30 @@
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+import googleIconImg from '../assets/images/google-icon.svg'
+
+import '../styles/auth.scss'
+import { Button } from '../components/Button'
+
+export function NewRoom() {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas"/>
+        <strong>Crie salas de Q&amp;A ao vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask"/>
+          <h2 className="new-room">Criar uma nova sala</h2>
+          <form>
+            <input type="text" placeholder="Nome da sala"/>
+            <Button type="submit">Criar sala</Button>
+          </form>
+          <p className="existing-room">Quer entrar em uma sala existente? <a href="#">clique aqui</a></p>
+
+        </div>
+      </main>
+    </div>
+  )
+}
