139ae33cab7203e5cbbb82b720dcb90c22f8857a
Wellington de Lima <wellington.map@hotmail.com>
2021-06-23 18:39:48 -0300

    add react router dom

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,15 @@ import googleIconImg from '../assets/images/google-icon.svg'
 
 import '../styles/auth.scss'
 import { Button } from '../components/Button'
+import { useHistory } from 'react-router'
 
 export function Home() {
+  const history = useHistory()
+
+  function navigateToNewRoom() {
+    history.push('/rooms/new')
+  }
+
   return (
     <div id="page-auth">
       <aside>
@@ -16,7 +23,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask"/>
-          <button className="create-room">
+          <button className="create-room" onClick={navigateToNewRoom}>
             <img src={googleIconImg} alt="Logo do Google"/>
             Crie sua sala com Google
           </button>
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,6 +1,7 @@
+import { Link } from 'react-router-dom'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
-import googleIconImg from '../assets/images/google-icon.svg'
 
 import '../styles/auth.scss'
 import { Button } from '../components/Button'
@@ -21,7 +22,7 @@ export function NewRoom() {
             <input type="text" placeholder="Nome da sala"/>
             <Button type="submit">Criar sala</Button>
           </form>
-          <p className="existing-room">Quer entrar em uma sala existente? <a href="#">clique aqui</a></p>
+          <p className="existing-room">Quer entrar em uma sala existente? <Link to='/'>clique aqui</Link></p>
 
         </div>
       </main>
